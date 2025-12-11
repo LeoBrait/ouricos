c6e03f4d4009fb3973f66146ca8043b2b411faf0
LeoBrait <leonardobrait@gmail.com>
2025-12-11 10:16:54 -0300

    wip

--- 03_multivariada.R
+++ 03_multivariada.R
@@ -1,23 +1,82 @@
 library(vegan)
+library(tidyverse)
+
+
+
 
 df <- read.csv("ouriquinho.csv")
-df$FatorCombinado <- interaction(df$estrato, df$ponto, sep = "_")
+
+
+df <- df %>%
+  mutate(
+    estrato = fct_relevel(
+      estrato,
+      c("praia_superior", "meio", "oceano_inferior")
+    ),
+    ponto = fct_relevel(
+      ponto,
+      c("direita", "esquerda")
+    )
+  ) %>%
+  mutate(
+    estrato = fct_recode(
+      estrato,
+      "Superior" = "praia_superior",
+      "Intermediário" = "meio",
+      "Inferior" = "oceano_inferior",
+    ),
+    ponto = fct_recode(
+      ponto,
+      "Leste" = "direita",
+      "Oeste" = "esquerda"
+    ),
+    label = interaction(ponto, replica, sep = " "),
+    eixo = interaction(estrato, ponto, replica, sep = " ")
+  )
+
+
+
+
 
 comm <- df[, c("satelite", "ourico", "pepino", "ofiuridio")]
 
 # remover amostras sem organismos
 comm2 <- comm[rowSums(comm) > 0, ]
 estrato2 <- df$estrato[rowSums(comm) > 0]
+eixo <- df$eixo[rowSums(comm) > 0]
+
+# criar um vetor com 
 
 # distância e clustering
 dist_bc <- vegdist(comm2, method = "bray")
 clust <- hclust(dist_bc, method = "average")
+perm <- adonis2(dist_bc ~ estrato2)
 
-plot(clust, labels = estrato2)
+R2 <- round(perm$R2[1], 3)
+pval <- perm$`Pr(>F)`[1]
 
-
-adonis2(dist_bc ~ estrato2)
+# Texto para colocar no título
+titulo <- paste0(
+  "Cluster (Bray-Curtis) — PERMANOVA: R² = ", R2,
+  ", p = ", format(pval, scientific = TRUE, digits = 3)
+)
+plot(
+  clust,
+  labels = eixo,
+  main = titulo,
+  xlab = "Amostras",
+  ylab = "Distância",
+  sub = ""
+)
 anosim(dist_bc, estrato2)
 
-anosim_interacao <- anosim(dist_bc, df$FatorCombinado[rowSums(comm) > 0], permutations = 9999)
-anosim_interacao
+
+plot(clust, labels = eixo, main = "Cluster das amostras - Distância de Bray-Curtis",
+     xlab = "Amostras", ylab = "Distância", sub = "")
+
+
+
+
+
+# ANOTAR RESULTADOS DO R2 E P-VALUE DA PERMANOVA NO GRÁFICO
+boxplot(dist_bc ~ estrato2)
